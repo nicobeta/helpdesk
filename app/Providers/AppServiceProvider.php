16b126362996a717a2849e1e23bd4178ef50cf9f
Nico Beta <nico1485@gmail.com>
2017-06-18 19:19:22 -0300

    Add basic multiple database support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $account = request('account');
+
+        Config::set('database.connections.account', array_merge(
+            Config::get('database.connections.' . Config::get('database.default')),
+            ['database'  => $account]
+        ));
     }
 }
--- app/Thread.php
+++ app/Thread.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    protected $connection = 'account';
+
     protected $fillable = ['reference', 'subject'];
 
     public function messages()
